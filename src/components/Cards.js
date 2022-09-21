fcb528ac961eacd69b4d6420bc4c2985a9f36d7b
Robert Jones <jones.robertbrendan@gmail.com>
2022-09-21 01:46:00 -0500

    'Updated color styles'

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Cards.css'
 import { Button } from './Button'
 import { BsGithub } from 'react-icons/bs';
 import { BiLinkExternal } from 'react-icons/bi'
 
-function Card({name, body, list}) {
+function Card({name, body, list, git, link}) {
+
+    
+
     return (
         <div className="card-container">
             <div className="card-title">
@@ -18,24 +21,13 @@ function Card({name, body, list}) {
                 {list}
             </div>
             <div className="card-icons">
-                <div  className="card-icon-item">
+                <div onClick={git} className="card-icon-item">
                     <BsGithub />
                 </div>
-                <div className="card-icon-item">
+                <div onClick={link} className="card-icon-item">
                     <BiLinkExternal />
                 </div>
             </div>
-            {/* <div className="card-btn">
-                {Button ? (
-                    <Link to='../../Resume.pdf' target='_blank' className='btn-link'>
-                        <Button buttonStyle='primary' buttonColor='btn-dark'>Code</Button>
-                    </Link>
-                ) : (
-                    <Link to='../../Resume.pdf' target='_blank' className='btn-link'>
-                        <Button buttonStyle='primary' buttonColor='btn-light'>Code</Button>
-                    </Link>
-                )}
-            </div> */}
         </div>
     )
 }
