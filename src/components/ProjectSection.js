a8bc7bf159e9129ae4ad5af86ab5afc300083e9f
Robert Jones <jones.robertbrendan@gmail.com>
2022-09-28 03:42:25 -0500

    'Updated responsiveness'

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -45,6 +45,9 @@ function ProjectSection({
                   <div className="small-cards-item">
                     <Cards name={sItemThree} body={sBodyThree} list={sListThree} git={sGitThree} link={sLinkThree}/>
                   </div>
+                  {/* <div className="small-cards-item">
+                    <Cards />
+                  </div> */}
                 </div>
 
                 <div className="small-card-group">
@@ -54,9 +57,6 @@ function ProjectSection({
                   <div className="small-cards-item">
                     <Cards name={sItemFive} body={sBodyFive} list={sListFive} git={sGitFive} link={sLinkFive}/>
                   </div>
-                  {/* <div className="small-cards-item">
-                    <Cards name={sItemSix} body={sBodySix} list={sListSix} git={sGitSix} link={sLinkSix}/>
-                  </div> */}
                 </div>
               </div>
         </div>
